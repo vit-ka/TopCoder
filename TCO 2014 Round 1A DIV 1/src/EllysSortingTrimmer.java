3cec18a52939dfda1328fb64bdb41db5f82be7b1
Vitaly Kalinkin <vitaly.kalinkin@gmail.com>
2014-04-26 17:41:57 +0400

    Solved EllysSortingTrimmer problem.

--- TCO 2014 Round 1A DIV 1/src/EllysSortingTrimmer.java
+++ TCO 2014 Round 1A DIV 1/src/EllysSortingTrimmer.java
@@ -1,10 +1,32 @@
-import java.util.*;
-import java.math.*;
-import static java.lang.Math.*;
+import java.util.Arrays;
 
 public class EllysSortingTrimmer {
 	
 	public String getMin(String S, int L) {
-		return null;
+
+        String result = S;
+
+        while (result.length() != L) {
+            String pref = result.substring(0, result.length() - L);
+            String suff = result.substring(result.length() - L);
+
+            suff = sortString(suff);
+
+            result = pref + suff;
+            result = result.substring(0, result.length() - 1);
+        }
+
+        result = sortString(result);
+
+        return result;
 	}
+
+    private String sortString(String result) {
+        char[] letters = result.toCharArray();
+        Arrays.sort(letters);
+        result = String.valueOf(letters);
+        return result;
+    }
+
+
 }
